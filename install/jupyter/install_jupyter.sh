abdbacda09aefb097ff5628911d7f61198676bbb
DisasterlyDisco <aleksander.r.m@protonmail.com>
2025-11-05 18:44:44 +0100

    Reduced the amount of python packages installed

--- install/jupyter/install_jupyter.sh
+++ install/jupyter/install_jupyter.sh
@@ -5,20 +5,10 @@ set -xe
 
 apt-get update && apt-get install -y \
     python3 \
-    python3-pip \
-    python3-venv \
-    python3-dev \
-    python3-setuptools \
-    python3-wheel \
-    && rm -rf /var/lib/apt/lists/*
+    python3-pip
+apt-get clean
+rm -rf /var/lib/apt/lists/*
 
 pip install --break-system-packages \
-    jupyter \
     jupyterlab \
-    notebook \
-    ipywidgets \
-    jupyter-contrib-nbextensions \
-    jupyter-nbextensions-configurator \
-    jupyterlab-git \
-    jupyterlab-lsp \
-    python-lsp-server[all]
+    notebook
